e91b6c770187238233cd9017ac55c3f02ef2e545
Wenish <Wenish@users.noreply.github.com>
2018-12-07 14:28:01 +0100

    update

--- AppTestAdpativeCardBug/MainPage.xaml.cs
+++ AppTestAdpativeCardBug/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using AdaptiveCards;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,38 @@ namespace AppTestAdpativeCardBug
         public MainPage()
         {
             this.InitializeComponent();
+            AdaptiveCard card = new AdaptiveCard();
+            card.Body.Add(new AdaptiveTextBlock()
+            {
+                Text = "Hello",
+                Size = AdaptiveTextSize.ExtraLarge
+            });
+
+            card.Body.Add(new AdaptiveImage()
+            {
+                Url = new Uri("http://adaptivecards.io/content/cats/1.png")
+            });
+
+            var datePicker = new AdaptiveDateInput()
+            {
+                Id = "123",
+                Placeholder = "123",
+                Value = "2017-09-20"
+            };
+            card.Body.Add(datePicker);
+
+            // serialize the card to JSON
+            string json = card.ToJson();
+
+            var renderer = new AdaptiveCards.Rendering.Uwp.AdaptiveCardRenderer();
+
+            var cardNew = AdaptiveCards.Rendering.Uwp.AdaptiveCard.FromJsonString(json);
+
+            var renderedAdaptiveCard = renderer.RenderAdaptiveCard(cardNew.AdaptiveCard);
+            if (renderedAdaptiveCard.FrameworkElement != null)
+            {
+                myGrid.Children.Add(renderedAdaptiveCard.FrameworkElement);
+            }
         }
     }
 }
